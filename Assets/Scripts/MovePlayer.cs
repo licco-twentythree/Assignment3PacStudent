9153bb2d5e4b29ad927ab683db2bb6d63b147344
licco-twentythree <licco23@gmail.com>
2022-09-18 10:53:44 +1000

    Added Simple Movement for Testing

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,47 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     [SerializeField] private int speed;
+    private string directionKey;
+
+    private string lastDirectionKey;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        HandleMovement();
+    }
 
-        transform.position = new Vector2(transform.position.x + (horizontalInput * speed * Time.deltaTime), transform.position.y + (verticalInput * speed * Time.deltaTime));
+    void HandleMovement()
+    {
+        directionKey = Input.inputString;
+        if (directionKey != "")
+        {
+            lastDirectionKey = directionKey;
+        }
+
+        switch (lastDirectionKey)
+        {
+            case "w":
+                transform.position = new Vector2(transform.position.x,
+                    transform.position.y + (speed * Time.deltaTime));
+                break;
+            case "s":
+                transform.position = new Vector2(transform.position.x,
+                    transform.position.y + (-speed * Time.deltaTime));
+                break;
+            case "a":
+                transform.position = new Vector2(transform.position.x + (-speed * Time.deltaTime),
+                    transform.position.y);
+                break;
+            case "d":
+                transform.position = new Vector2(transform.position.x + (speed * Time.deltaTime),
+                    transform.position.y);
+                break;
+        }
     }
 }
